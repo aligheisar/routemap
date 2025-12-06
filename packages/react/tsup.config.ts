5540a9a2e8077ab0b4ee83d13b769c85cf0dd92e
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-12-06 15:04:27 +0330

    update package.json for exporting, adding tsup config

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  treeshake: true,
+
+  outDir: "dist",
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".js",
+    };
+  },
+});
